5c50f2e22f71812f6bc94a0630eb37e377864746
Israelfluz <israelfaria.gtr@gmail.com>
2019-07-30 15:43:54 -0300

    Funções de ativação - Implemantação

--- redes neurais artificiais/funcoes_ativacao.py
+++ redes neurais artificiais/funcoes_ativacao.py
@@ -13,18 +13,21 @@ def stepFunction(soma):
 def sigmoidFunction(soma):
     return 1 / (1 + np.exp(-soma))
 
-# Criando a função tangente hiperbólica
+# Criando a função tangente hiperbólica - utilizada também para classificação
 def tahnFunction(soma):
     return (np.exp(soma) - np.exp(-soma)) / (np.exp(soma) + np.exp(-soma))
 
+# Criando a função ReLU (rectified linear units) retorna valores maiores ou iguas a 0 
 def reluFunction(soma):
     if soma >= 0:
         return soma
     return 0
 
+# Criando a função Linear
 def linearFunction(soma):
     return soma
 
+# Criando a função Softmax - Ela retorna probabilidades quando se tem problema com mais de duas classes
 def softmaxFunction(x):
     ex = np.exp(x)
     return ex / ex.sum()
@@ -38,9 +41,13 @@ teste = sigmoidFunction(-0.358)
 # Realizando o teste da função tangente hiperbólica
 teste = tahnFunction(-0.358)
 
-
+# Realizando o teste da função ReLU (rectified linear units)
 teste = reluFunction(0.358)
+
+# Realizando o teste da função linear
 teste = linearFunction(-0.358)
+
+# Realizando o teste da função Softmax
 valores = [7.0, 2.0, 1.3]
 print(softmaxFunction(valores))
 
