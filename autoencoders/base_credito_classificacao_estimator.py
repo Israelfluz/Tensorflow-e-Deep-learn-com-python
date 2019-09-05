9f2f9b2b4a9b871b5dfabc35e895c9b18de20988
Israelfluz <israelfaria.gtr@gmail.com>
2019-09-05 16:51:47 -0300

    Base de crédito classificação com estimator

--- autoencoders/base_credito_classificacao_estimator.py
+++ autoencoders/base_credito_classificacao_estimator.py
@@ -1,6 +1,6 @@
 import pandas as pd
 base = pd.read_csv('credit-data.csv')
-base.haed()
+base.head()
 
 base.shape
 
@@ -9,17 +9,59 @@ base = base.drop('i#clientid', axis = 1)
 base.head()
 
 
-base.dropna() # Apagando os registros que não tiveram seus valores preenchidos (NAN)
+base = base.dropna() # Apagando os registros que não tiveram seus valores preenchidos (NAN)
 base.shape
 
 # Realizando o escalonamento e padroizando(standardscaler)
-from sklearn.processing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 scaler_x = StandardScaler()
 base[['income', 'age', 'loan']] = scaler_x.fit_transform(base[['income', 'age', 'loan']])
-base.haed()
+base.head()
 
-# Criando a variável com os atributos previsores e retirando a classe que mostra quem vai pagar e quem não
-x = base.drop('c#default', axis = 1)
+# Variável que recebe os atributos previsores. 
+x = base.drop('c#default', axis = 1) # c#default retira a classe (coluna) que mostra quem vai pagar e quem não
+
+# Variável que recebe o C#default que o conjunto de 0 e 1
 y = base['c#default']
 
-x.haed()              
+x.head()
+
+y.head()
+
+x.columns
+
+# Importando o framework 
+import tensorflow as tf
+colunas = [tf.feature_column.numeric_column(key = column) for column in x.columns]
+
+# Visualizando a variável colunas criada
+print(colunas[2]) # Basta colocar o número da coluna
+
+
+# Divisão da base de dados de treinamento e teste
+from sklearn.model_selection import train_test_split 
+x_treinamento, x_teste, y_treinamento, y_teste = train_test_split(x, y, test_size = 0.3, random_state = 0)
+
+
+x_treinamento.shape
+
+
+x_teste.shape
+
+# Criando a função para o treinamento com estimators
+funcao_teste = tf.estimator.inputs.pandas_input_fn(x = x_treinamento,
+                                                         y = y_treinamento,
+                                                         batch_size = 8,
+                                                         num_epochs = None,
+                                                         shuffle = True) 
+
+# Criando a rede neural
+classificador = tf.estimator.DNNClassifier(feature_columns = colunas, hidden_units = [4, 4])
+classificador.train(input_fn = funcao_treinamento, steps = 1000)
+
+
+# Criando a variável para fazer avaliação na base de dados de teste
+metricas_teste = classificador.evaluate(input_fn = funcao_teste, steps = 1000)
+
+metricas_teste  
+           
