886ac1d37c1e6e1331c073472078791daf0492f6
Israelfluz <israelfaria.gtr@gmail.com>
2019-09-05 11:48:19 -0300

    Base de crédito classificação com estimator

--- autoencoders/base_credito_classificacao_estimator.py
+++ autoencoders/base_credito_classificacao_estimator.py
@@ -4,17 +4,21 @@ base.haed()
 
 base.shape
 
+# Apagandoa a coluna i#clientid pois seu conteúdo não ajudará nos algoritmos
 base = base.drop('i#clientid', axis = 1)
 base.head()
 
-base.dropna()
+
+base.dropna() # Apagando os registros que não tiveram seus valores preenchidos (NAN)
 base.shape
 
+# Realizando o escalonamento e padroizando(standardscaler)
 from sklearn.processing import StandardScaler
 scaler_x = StandardScaler()
 base[['income', 'age', 'loan']] = scaler_x.fit_transform(base[['income', 'age', 'loan']])
 base.haed()
 
+# Criando a variável com os atributos previsores e retirando a classe que mostra quem vai pagar e quem não
 x = base.drop('c#default', axis = 1)
 y = base['c#default']
 
